0eda6924e30a24127a52ca1f2209510e4897e023
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2024-08-15 16:05:06 +0530

    added script

--- create_images.sh
+++ create_images.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 git pull
-docker build -t rbeniwal26119/basic-django
-docker push
+docker build -t rbeniwal26119/basic-django .
+docker push rbeniwal26119/basic-django
 
 docker run -p 8000:8000 rbeniwal26119/basic-django:latest
