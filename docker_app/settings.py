2e4e83129ddcf96d75b09ce5c8646d471ae68989
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2024-08-15 16:23:30 +0530

    added script

--- docker_app/settings.py
+++ docker_app/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/starwars/static/'
 
-STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static_files')
 
