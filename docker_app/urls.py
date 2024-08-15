1e5d227885181d95fcec714eedbaaee15253210e
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2024-08-15 15:57:03 +0530

    added starwars/

--- docker_app/urls.py
+++ docker_app/urls.py
@@ -33,13 +33,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("first_app.urls")),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+    path('starwars/admin/', admin.site.urls),
+    path("starwars/", include("first_app.urls")),
+    re_path(r'^starwars/swagger(?P<format>\.json|\.yaml)$',
          schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+    re_path(r'^starwars/swagger/$', schema_view.with_ui('swagger',
          cache_timeout=0), name="schema-swagger"),
-    re_path(r'^redoc/$', schema_view.with_ui("redoc",
+    re_path(r'^starwars/redoc/$', schema_view.with_ui("redoc",
     cache_timeout=0), name="schema-redoc"),
-    path('tinymce/', include('tinymce.urls'))
+    path('starwars/tinymce/', include('tinymce.urls'))
 ]
